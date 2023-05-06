36107bf545cf6a06cadba8295ca4f68bb4619a1f
ilgar <ilgarasd@gmail.com>
2023-05-05 22:37:36 -0400

    day32 custom classes

---
+++ src/day37_a_abstraction/Rivian.java
@@ -0,0 +1,14 @@
+package day37_a_abstraction;
+
+public class Rivian extends ElectricCar{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void charging() {
+
+    }
+    // this starts by the voice control
+}
---
+++ src/day37_a_abstraction/Tesla.java
@@ -0,0 +1,14 @@
+package day37_a_abstraction;
+
+public class Tesla extends ElectricCar{
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void charging() {
+
+    }
+    // this starts by the card
+}
